9186c99f1fd72355e59f392ab8493671c9c85345
Thomas Kluyver <thomas.kluyver@xfel.eu>
2024-05-07 14:54:01 +0100

    Add bad pixel flags to extra.calibration

--- src/extra/calibration.py
+++ src/extra/calibration.py
@@ -3,6 +3,7 @@ import re
 from collections.abc import Mapping
 from dataclasses import dataclass, field, replace
 from datetime import date, datetime, time, timezone
+from enum import IntFlag
 from functools import lru_cache
 from pathlib import Path
 from typing import Dict, List, Optional, Union
@@ -962,3 +963,30 @@ class JUNGFRAUConditions(ConditionsBase):
             del cond["Gain mode"]
 
         return cond
+
+
+class BadPixels(IntFlag):
+    """Bad pixel reasons, as used in masks in corrected detector data"""
+    OFFSET_OUT_OF_THRESHOLD  = 1 << 0
+    NOISE_OUT_OF_THRESHOLD   = 1 << 1
+    OFFSET_NOISE_EVAL_ERROR  = 1 << 2
+    NO_DARK_DATA             = 1 << 3
+    CI_GAIN_OF_OF_THRESHOLD  = 1 << 4
+    CI_LINEAR_DEVIATION      = 1 << 5
+    CI_EVAL_ERROR            = 1 << 6
+    FF_GAIN_EVAL_ERROR       = 1 << 7
+    FF_GAIN_DEVIATION        = 1 << 8
+    FF_NO_ENTRIES            = 1 << 9
+    CI2_EVAL_ERROR           = 1 << 10
+    VALUE_IS_NAN             = 1 << 11
+    VALUE_OUT_OF_RANGE       = 1 << 12
+    GAIN_THRESHOLDING_ERROR  = 1 << 13
+    DATA_STD_IS_ZERO         = 1 << 14
+    ASIC_STD_BELOW_NOISE     = 1 << 15
+    INTERPOLATED             = 1 << 16
+    NOISY_ADC                = 1 << 17
+    OVERSCAN                 = 1 << 18
+    NON_SENSITIVE            = 1 << 19
+    NON_LIN_RESPONSE_REGION  = 1 << 20
+    WRONG_GAIN_VALUE         = 1 << 21
+    NON_STANDARD_SIZE        = 1 << 22
