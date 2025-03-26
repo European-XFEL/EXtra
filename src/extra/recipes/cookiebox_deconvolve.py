e05d81e6182eab394933b67fa8f63af60b3f15d1
Danilo Ferreira de Lima <danilo.enoque.ferreira.de.lima@xfel.de>
2025-03-26 16:43:29 +0100

    Added TOFAnalogResponse.

--- src/extra/recipes/cookiebox_deconvolve.py
+++ src/extra/recipes/cookiebox_deconvolve.py
@@ -440,16 +440,18 @@ class TOFResponse(SerializableMixin):
     ```
 
     Args:
-      counting_threshold: Threshold to use when identifying edges.
+      threshold: Threshold to use when identifying edges.
       n_peaks: Number of peaks to select, if positive. Otherwise no peak selection is made.
       n_samples: Total number of samples use for impulse response.
       n_filter: Require no edges to be found before sample n_filter and after n_samples-n_filter.
+      roi: Region of interest. Keep None to use the full trace.
     """
-    def __init__(self, counting_threshold: int=-7,
-                 n_peaks: Optional[int]=None,
+    def __init__(self, threshold: int=-7,
+                 n_peaks: Optional[int]=1,
                  n_samples: int=400,
-                 n_filter: int=100):
-        self.counting_threshold = counting_threshold
+                 n_filter: int=100,
+                 roi: Optional[slice]=None):
+        self.threshold = threshold
         if n_peaks is None:
             n_peaks = -1
         self.n_peaks = n_peaks
@@ -458,12 +460,13 @@ class TOFResponse(SerializableMixin):
         self.h = None
         self.h_digital = None
         self.h_analog = None
+        self.roi = roi
         self._version = 1
         self._all_fields = [
                             "h",
                             "h_digital",
                             "h_analog",
-                            "counting_threshold",
+                            "threshold",
                             "n_samples",
                             "n_filter",
                             "n_peaks",
@@ -497,11 +500,14 @@ class TOFResponse(SerializableMixin):
         # get pulse data
         logging.info("Get pulse data ...")
         this_tof_data = tof.pulse_data(pulse_dim='pulseIndex')
+        if self.roi is not None:
+            this_tof_data = this_tof_data.isel(sample=self.roi)
+
         # get edges
         logging.info("Get edges ...")
         edges_pos, edges_amp = tof.find_edge_array(this_tof_data,
                                                    labelled=False,
-                                                   threshold=self.counting_threshold)
+                                                   threshold=self.threshold)
 
         if edges_pos.shape[-1] == 0:
             raise ValueError(f"No peaks found.")
@@ -523,7 +529,7 @@ class TOFResponse(SerializableMixin):
 
         # find edges and build both digital and analog response functions
         logging.info("Build digital signal ...")
-        edges = tof.find_edges(this_tof_data, threshold=self.counting_threshold)
+        edges = tof.find_edges(this_tof_data, threshold=self.threshold)
         ts1 = np.arange(0, self.n_samples+1)
         h_digital, _ = np.histogram(edges.edge, bins=ts1)
         h_digital = h_digital.astype(float)/np.amax(h_digital)
@@ -547,37 +553,37 @@ class TOFResponse(SerializableMixin):
         aligned /= np.amax(aligned)
         self.h = aligned
 
-    def get_response(self, type: str="aligned", reflection_period: List[int]=list(), reflection_amplitude: List[float]=list()):
+    def get_response(self, kind: str="aligned", reflection_period: List[int]=list(), reflection_amplitude: List[float]=list()):
         """
         Returns the instrument response.
 
         Args:
-          type: Which version of the isntrument response to retrieve.
+          kind: Which version of the isntrument response to retrieve.
                 "aligned" provides the MCP response, which is edge-aligned.
                 "digital" is the digital response.
                 "analog" is the analog response.
           reflection_period: Simulate reflection with these distances.
           reflection_amplitude: Amplitudes of the reflections.
         """
-        if type == "aligned":
+        if kind == "aligned":
             h = self.h
-        elif type == "digital":
+        elif kind == "digital":
             h = self.h_digital
-        elif type == "analog":
+        elif kind == "analog":
             h = self.h_analog
         else:
-            raise ValueError(f"Unknown type={type}.")
+            raise ValueError(f"Unknown kind={kind}.")
         if len(reflection_period) > 0:
             h = fftconvolve(h, comb(reflection_period, reflection_amplitude), mode='same')
         return h
 
-    def plot(self):
+    def plot(self, kind: str="aligned"):
         """
         Plot impulse response.
         """
         import matplotlib.pyplot as plt
         fig = plt.figure(figsize=(10, 8))
-        plt.plot(np.arange(-self.n_filter, self.n_samples-self.n_filter), self.h, lw=2)
+        plt.plot(np.arange(-self.n_filter, self.n_samples-self.n_filter), self.get_response(kind), lw=2)
         plt.xlabel("Samples")
         plt.ylabel("Intensity [a.u.]")
 
@@ -636,6 +642,255 @@ class TOFResponse(SerializableMixin):
         original = -tof.pulse_data(pulse_dim='pulseIndex').sel(sample=np.s_[0:400])
         # retrieve impulse response from a file
         response = TOFResponse.from_file("tof4.h5")
+        # apply it
+        result = response.apply(original)
+        # plot it
+        plt.plot(result.isel(trainId=0, pulseIndex=0), lw=2, label="Deconvolved")
+        plt.plot(original.isel(trainId=0, pulseIndex=0), lw=2, label="Original")
+        plt.show()
+        ```
+
+        Args:
+          tof_trace: The eTOF data as a DataArray returned by `AdqRawChannel` with shape (pulses, samples), or
+               a numpy array of shape (n_pulses, n_samples)
+          Lambda: The regularization strength.
+          n_iter: Number of deconvoluton iterations.
+          method: Set to "matrix" for matrix multplications (faster for shorter traces, ie len(trace) < 400).
+                  Use "fft" for the FFT-based method. Warning: the two methods provide similar, but *not identcal* results.
+                  Use "standard" for  standard deconvolution.
+
+        Returns: The deconvolved data as a DataArray.
+        """
+        norm = 1
+        if isinstance(tof_trace, np.ndarray):
+            if len(tof_trace.shape) == 1:
+                norm = np.max(tof_trace)
+                original = xr.DataArray(tof_trace/norm, dims=('sample',))
+            else:
+                original = xr.DataArray(tof_trace, dims=('pulse', 'sample'))
+                norm = np.max(original.data, axis=0, keepdims=True)
+                original.data /= norm
+        elif isinstance(tof_trace, xr.DataArray):
+            original = tof_trace.copy()
+            norm = np.max(original.data, axis=0, keepdims=True)
+            original.data /= norm
+        else:
+            raise ValueError("Expect `tof_trace` to be a numpy array or xarray DataArray.")
+        h = self.get_response(reflection_period=reflection_period, reflection_amplitude=reflection_amplitude)
+        if method == "tv_matrix":
+            result_trace = tv_deconvolution(original.data, h=h, Lambda=Lambda, n_iter=n_iter, n_shift=self.n_filter)
+        elif method == "standard":
+            result_trace = std_deconvolution(original.data, h=h, n_shift=self.n_filter)
+        elif method == "tv_fft":
+            result_trace = tv_deconvolution_fft(original.data, h=h, Lambda=Lambda, n_iter=n_iter, n_shift=self.n_filter)
+        else:
+            raise ValueError("Unknown method.")
+        result = original
+        result.data = result_trace*norm
+        return result
+
+class TOFAnalogResponse(SerializableMixin):
+    """
+    Given a run with the Cookiebox in analog mode, obtain its impulse response.
+
+    Example usage:
+    ```
+    tof_id = 4
+    channel = "3_A"
+    digitizer = "SQS_DIGITIZER_UTC4/ADC/1:network"
+    energy_source = "SA3_XTD10_MONO/MDL/PHOTON_ENERGY"
+    calib_run = open_run(proposal=900485, run=320)
+    scan = Scan(calib_run[energy_source, "actualEnergy.value"],
+                resolution=2, intra_step_filtering=1)
+    calib_run = calib_run.select([digitizer, energy_source], require_all=True)
+    tof = AdqRawChannel(calib_run,
+                        channel=channel,
+                        digitizer=digitizer,
+                        first_pulse_offset=23300,
+                        single_pulse_length=600,
+                        interleaved=True,
+                        baseline=np.s_[:20000],
+                        )
+    response = TOFAnalogResponse(roi=np.s_[75:])
+    response.setup(tof, scan)
+    response.to_file("tof4.h5")
+    h = response.get_response()
+    ```
+
+    This could also be coupled with the `CookieboxCalibration` object to deconvolve
+    and denoise spectra before calibration.
+
+    ```
+    # open a new run
+    run = open_run(proposal=900485, run=349)
+    # get the calibration constants
+    cal = CookieboxCalibration.from_file("my_calibration.h5")
+    # load the trace for this new run, exactly as required for calibration
+    trace = cal.load_trace(run)
+    # apply the deconvolution using this TOFResponse object
+    response = TOFAnalogResponse.from_file("tof4.h5")
+    trace.loc[dict(tof=4)] = response.apply(trace.sel(tof=4))
+    # now apply the calibration:
+    spectrum = cal.calibrate(trace)
+    ```
+
+    Args:
+      roi: Region of interest. Keep None to use the full trace.
+      n_samples: Total number of samples use for impulse response.
+    """
+    def __init__(self,
+                 roi: slice,
+                 n_samples: int=300,
+                 n_filter: int=75,
+                 ):
+        self.roi = roi
+        self.n_samples = n_samples
+        self.n_filter = n_filter
+        self.h = None
+        self._version = 1
+        self._all_fields = [
+                            "h",
+                            "n_samples",
+                            "n_filter",
+                            "_version",
+                           ]
+
+    def _asdict(self):
+        """
+        Return a serializable dict.
+        """
+        d = {k: v for k, v in self.__dict__.items() if k in self._all_fields}
+        return d
+    def _fromdict(self, all_data):
+        """
+        Actions to do after loading from file.
+        """
+        for k, v in all_data.items():
+            setattr(self, k, v)
+
+    def setup(self,
+              tof: AdqRawChannel,
+              scan: Scan,
+              ):
+        """
+        Given a `AdqRawChannel` object, and an energy scan object
+        calculate this eTOF's impulse response function,
+        assuming the corresponding run contins a scan over monochromated beams.
+
+        Args:
+          tof: `AdqRawChannel` used to access data for a given eTOF.
+          scan: `Scan` used to detect which regions cotain which energies.
+        """
+        
+        # get pulse data
+        logging.info("Get pulse data ...")
+        this_tof_data = -tof.pulse_data(pulse_dim='pulseIndex')
+        if self.roi is not None:
+            this_tof_data = this_tof_data.isel(sample=self.roi)
+        this_tof_data = this_tof_data.isel(sample=np.s_[:self.n_samples])
+        this_tof_data = this_tof_data.unstack('pulse')
+
+        # get means
+        logging.info("Get energy means ...")
+        h_axis = np.arange(self.n_samples)
+        h = list()
+        for k, e in enumerate(scan.positions):
+            mono_data = this_tof_data.sel(trainId=scan.positions_train_ids[k]).mean('trainId').mean('pulseIndex').to_numpy()
+            mono_data /= np.amax(mono_data)
+            m = np.argmax(mono_data)
+            xi = np.arange(self.n_samples) - m + self.n_filter
+            h += [np.interp(h_axis, xi, mono_data)]
+        h = np.stack(h, axis=0)
+        h_unc = h.std(0)
+        h = h.mean(0)
+        self.h = h
+        self.h_unc = h_unc
+
+    def get_response(self, reflection_period: List[int]=list(), reflection_amplitude: List[float]=list()):
+        """
+        Returns the instrument response.
+
+        Args:
+          kind: Which version of the isntrument response to retrieve.
+                "aligned" provides the MCP response, which is edge-aligned.
+                "digital" is the digital response.
+                "analog" is the analog response.
+          reflection_period: Simulate reflection with these distances.
+          reflection_amplitude: Amplitudes of the reflections.
+        """
+        h = self.h
+        if len(reflection_period) > 0:
+            h = fftconvolve(h, comb(reflection_period, reflection_amplitude), mode='same')
+        return h
+
+    def plot(self):
+        """
+        Plot impulse response.
+        """
+        import matplotlib.pyplot as plt
+        fig = plt.figure(figsize=(10, 8))
+        plt.plot(np.arange(self.n_samples), self.h, lw=2, label="Impulse response")
+        plt.fill_between(np.arange(self.n_samples), self.h-self.h_unc, self.h+self.h_unc,
+                         facecolor='r', alpha=0.5, label="68% CL")
+        plt.xlabel("Samples")
+        plt.ylabel("Intensity [a.u.]")
+        plt.legend(frameon=False)
+
+    def apply(self, tof_trace: Union[xr.DataArray, np.ndarray],
+              Lambda: float=0.01,
+              n_iter: int=4000,
+              reflection_period: List[int]=list(),
+              reflection_amplitude: List[float]=list(),
+              method: str="matrix") -> xr.DataArray:
+        """
+        Apply TV-deconvolution on TOF data taken in *analog* mode,
+        assuming the given parameter setting and using the Lambda parameter
+        as a denoising strength.
+
+        This could be coupled with the `CookieboxCalibration` object to deconvolve
+        and denoise spectra before calibration.
+
+        ```
+        # open a new run
+        run = open_run(proposal=900485, run=349)
+        # get the calibration constants
+        cal = CookieboxCalibration.from_file("my_calibration.h5")
+        # load the trace for this new run, exactly as required for calibration
+        trace = cal.load_trace(run)
+        # apply the deconvolution using this TOFResponse object
+        response = TOFAnalogResponse.from_file("tof4.h5")
+        trace.loc[dict(tof=4)] = response.apply(trace.sel(tof=4))
+        # now apply the calibration:
+        spectrum = cal.calibrate(trace)
+        ```
+
+        If the raw trace is desired, this could be used independetly from the calibration
+        object simply to mprove the trace resolution.
+
+        Example of usage without the calibration:
+        ```
+        ts = "SQS_RR_UTC/TSYS/TIMESERVER:outputBunchPattern"
+        digitizer = 'SQS_DIGITIZER_UTC4/ADC/1:network'
+        digitizer_ctl = 'SQS_DIGITIZER_UTC4/ADC/1:network'
+        tof_id = 4
+        channel = "3_A"
+        calib_run = open_run(proposal=900485, run=509).select([ts,
+                                                               digitizer, digitizer_ctl
+                                                               ], require_all=True)
+        # set up AdqRawChannel object to read data from each eTOF
+        tof = AdqRawChannel(run,
+                            channel=channel,
+                            digitizer=digi,
+                            first_pulse_offset=23300,
+                            single_pulse_length=600,
+                            interleaved=True,
+                            baseline=np.s_[:20000],
+                            )
+        # fetch data
+        # select only first samples, since we don't care about the full trace
+        original = -tof.pulse_data(pulse_dim='pulseIndex').sel(sample=np.s_[0:400])
+        # retrieve impulse response from a file
+        response = TOFAnalogResponse.from_file("tof4.h5")
         # apply it
         result = response.apply(original)
         # plot it
