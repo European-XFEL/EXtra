8eea1a8cd016b7f82e12ac1147e300f03bc302a7
Danilo Ferreira de Lima <danilo.enoque.ferreira.de.lima@xfel.de>
2025-05-24 12:31:53 +0200

    Add roi in Gotthard calibration.

--- src/extra/recipes/grating.py
+++ src/extra/recipes/grating.py
@@ -208,7 +208,7 @@ class Grating1DCalibration(SerializableMixin):
         # average over pulses
         self.calibration_data = np.mean(self.calibration_data, axis=1)
         self.calibration_unc = np.mean(np.mean(self.calibration_unc, axis=1), axis=0)
-        self.calibration_mask = np.array([True for _ in self.calibration_data.shape[0]])
+        self.calibration_mask = np.array([True for _ in range(self.calibration_data.shape[0])])
 
     def mask_calibration_point(self, energy: float, mask: bool=False, tol: float=1.0):
         """
