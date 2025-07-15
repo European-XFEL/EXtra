fc1979667824d42e138dd1e055deef3c12f351f4
Thomas Kluyver <thomas.kluyver@xfel.eu>
2025-07-15 15:10:04 +0100

    Load default exfel-python module
    Co-authored-by: James Wrigley <JamesWrigley@users.noreply.github.com>

--- docs/install-extra.sh
+++ docs/install-extra.sh
@@ -10,7 +10,7 @@ source /usr/share/Modules/init/bash
 
 # Always install into the current environment
 module load exfel pixi
-module load exfel exfel-python/202502
+module load exfel exfel-python
 
 pixi update euxfel-extra --manifest-path ${PIXI_PROJECT_ROOT}
 pixi install --manifest-path ${PIXI_PROJECT_ROOT}
