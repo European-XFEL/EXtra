62db83d5554e3f9f46eba63a5782783c116a219b
Thomas Kluyver <thomas.kluyver@xfel.eu>
2026-09-01 17:04:25 +0100

    Add toolbox-sxp to install-extra.sh
    Extending the nightly automatic updating

--- docs/install-extra.sh
+++ docs/install-extra.sh
@@ -12,5 +12,5 @@ source /usr/share/Modules/init/bash
 module load exfel pixi
 module load exfel exfel-python
 
-pixi update euxfel-extra --manifest-path ${PIXI_PROJECT_ROOT}
+pixi update euxfel-extra toolbox-sxp --manifest-path ${PIXI_PROJECT_ROOT}
 pixi install --manifest-path ${PIXI_PROJECT_ROOT}
